554729157579fed00afe6be36ad3871ffbdda91c
Raunit Raj <raunit.raj@pal.tech>
2025-11-14 12:22:12 +0530

    for procedure verification

---
+++ target/compiled/T20_DBT_PROJECT_TESTING_1/models/STAGES/RAW/STG_RAW_TEAM2.sql
@@ -0,0 +1,13 @@
+
+
+WITH source_data AS (
+    SELECT 
+        "TEAMID",
+  "TEAMNAME",
+  "FILENAME",
+  "LOAD_TIMESTAMP",
+        CONVERT_TIMEZONE('UTC', CURRENT_TIMESTAMP())::TIMESTAMP_NTZ AS _inserted_at_
+    FROM RAUNIT_T20_DBT.T20_RAW.TEAM
+)
+
+select * from source_data;
